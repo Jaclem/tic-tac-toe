960dd3c0a8dd487d1b6e5b63c3eb54dfc42f65fe
Joshua Clement <jaclem12@gmail.com>
2022-12-22 21:35:13 -0500

    Added HTML elements to create the game board and put everything inside the transitionStage() function

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-const mainScreen = document.getElementById('main-modal');
-const addAI = document.getElementById('add-ai');
-const addPlayers = document.getElementById('add-players');
-
 const transitionStage = () => {
   const startBtn = document.getElementById('start-btn');
   const hiddenBtns = document.getElementById('hidden-buttons');
+  const mainScreen = document.getElementById('main-modal');
+  const addAI = document.getElementById('add-ai');
+  const addPlayers = document.getElementById('add-players');
 
+  // Adds a smooth transition to the main screen when "Start Game" button is clicked
   mainScreen.classList.add('smooth-transition');
   startBtn.remove();
   hiddenBtns.classList.add('visible');
-}
 
-
-function startWithAI() {
-  createGame();
-
-}
-
-function createGame() {
-  const mainGame = document.getElementById('main-game');
-  const gameContainer = document.createElement('div');
-  gameContainer.classList.add('game-container');
-  mainGame.classList.add('visible');
-
-  mainGame.appendChild(gameContainer);
-
-  for(let i = 0; i <= 8; i++){
-    const divs = document.createElement('div');
-    divs.classList.add('game-grid');
-    gameContainer.appendChild(divs);
+  // user clicks AI Bot button to add a bot that plays them in Tic Tac Toe
+  function startWithAI() {
+    const gridItem = document.querySelectorAll('.game-grid');
+    createGame();
+  
+    gridItem.forEach(item => {
+      item.addEventListener('click', ()=> {
+        item.innerHTML = "X"
+      });
+    });
   }
+
+  // Adds visibility to the mainGame element allowing you to interact with the game board
+  function createGame() {
+    const mainGame = document.getElementById('main-game');
+    mainGame.classList.add('visible');
+  }
+
+  // Does nothing yet
+  const gameBoard = () => {
+
+  }
+
+  addAI.addEventListener('click', ()=> {startWithAI();});
 }
 
-const gameBoard = () => {
 
-}
 
-addAI.addEventListener('click', ()=> {startWithAI();});
