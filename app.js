a6b290748f85f311a13174e1e3b7a69b47d1dba0
Joshua Clement <jaclem12@gmail.com>
2022-12-24 16:53:03 -0500

    Added some new functionality to the input area and added another factory function for player names

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const gameBoard = () => {
   const mainScreen = document.getElementById('main-screen');
   const addAI = document.getElementById('add-ai');
   const addPlayers = document.getElementById('add-players');
+  const addBtn = document.getElementById('add-names');
 
   let clicked = false;
 
-  addPlayers.addEventListener('click', () => {
-
-    if(!clicked){
-      addForm();
+  const playerNames = (player1, player2) => {
+    return {
+      test() {
+        console.log(`${player1} ${player2}`);
+      }
+      
     }
-    
-    clicked = true;
-  });
+  }
   
-  function addForm() {
+  const addForm = (() => {
     const form = document.createElement('form');
     const frstHeader = document.createElement('label');
     const scndHeader = document.createElement('label');
     const frstPlayer = document.createElement('input');
     const scndPlayer = document.createElement('input');
     const addBtn = document.createElement('button');
+
 
     form.className = 'player-form';
     frstHeader.className = 'name-label';
@@ -33,14 +35,47 @@ const gameBoard = () => {
     frstHeader.textContent = 'Player 1';
     scndHeader.textContent = 'Player 2';
     addBtn.textContent = 'Add';
+    
+    addBtn.setAttribute('id', 'add-names');
 
-    form.append(frstHeader, frstPlayer, scndHeader, scndPlayer, addBtn);
-    mainScreen.appendChild(form);
+    // appends all the elements that were created to the form
+    const createForm = () => {
+      form.append(frstHeader, frstPlayer, scndHeader, scndPlayer, addBtn);
+      mainScreen.appendChild(form);
+    }
 
-    return {form};
-  }
+    addBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const players = playerNames(frstPlayer.value, scndPlayer.value);
+      players.test();
+      form.remove();
+      clicked = false;
+    })
+
+    return {createForm};
+  })();
+
+  // Event Listeners //
+
+  // checks if the button has been clicked and if not it adds the form
+  // if it has been clicked it removes the form
+  addPlayers.addEventListener('click', () => {
+    const form = document.querySelector('.player-form');
+
+    if(!clicked){
+      addForm.createForm();
+      clicked = true;
+    } else {
+      form.remove();
+      clicked = false;
+    }
+  });
 
 
+
+  // addBtn.addEventListener('click', (e) => {
+  //   console.log()
+  // })
 
 }
 
